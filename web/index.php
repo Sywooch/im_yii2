423371f9918611c974895c73770995e7d4f8a200
Dmitry Khlebov <dmvolt77@gmail.com>
2017-04-25 14:44:36 +0700

    Отключен режим разработчика

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
 
 // количество материалов на странице по умолчанию
 defined('DEFAULT_FRONT_PAGE_SIZE') or define('DEFAULT_FRONT_PAGE_SIZE', 10);
